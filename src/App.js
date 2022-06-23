46599a0d8482c03a656964969bbb8e4d8cd187e1
hyunsang-ahn <happylovetkd@naver.com>
2022-06-23 21:32:50 +0900

    styling react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+//SASSCOMPONENT 추가
+import SassComponent from "./SassComponent";
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
